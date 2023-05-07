fe28338c8f90fb582717753454b1d7d968aef374
Abel Salgado Romero <romeroab@vmware.com>
2023-05-07 20:21:28 +0200

    Add '.editorconfig' and apply formatting

--- src/main/java/net/springio/scg/with/style/GatewayWithStyleApplication.java
+++ src/main/java/net/springio/scg/with/style/GatewayWithStyleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayWithStyleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayWithStyleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWithStyleApplication.class, args);
+    }
 
 }
