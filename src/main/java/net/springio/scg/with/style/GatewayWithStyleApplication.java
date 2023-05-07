7342d528901739264d48135ee2ebd06ee9265691
Abel Salgado Romero <romeroab@vmware.com>
2023-05-07 19:26:12 +0200

    Initial commit: blank project + README

---
+++ src/main/java/net/springio/scg/with/style/GatewayWithStyleApplication.java
@@ -0,0 +1,13 @@
+package net.springio.scg.with.style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWithStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayWithStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/springio/scg/with/style/GatewayWithStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springio.scg.with.style;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWithStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
