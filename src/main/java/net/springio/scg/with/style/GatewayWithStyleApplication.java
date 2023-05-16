977b8926f02ca5a3d45b1fcef67016188456caa9
Abel Salgado Romero <abelromero@gmail.com>
2023-05-16 19:06:04 +0200

    Add non-committed local configuration via "local" profile

--- src/main/java/net/springio/scg/with/style/GatewayWithStyleApplication.java
+++ src/main/java/net/springio/scg/with/style/GatewayWithStyleApplication.java
@@ -1,13 +1,16 @@
 package net.springio.scg.with.style;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GatewayWithStyleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayWithStyleApplication.class, args);
+        new SpringApplicationBuilder()
+            .sources(GatewayWithStyleApplication.class)
+            .profiles("local")
+            .run(args);
     }
 
 }
--- src/test/java/net/springio/scg/with/style/ActuatorGatewayRoutesTests.java
+++ src/test/java/net/springio/scg/with/style/ActuatorGatewayRoutesTests.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
+@ActiveProfiles("local")
 @AutoConfigureWebTestClient
 class ActuatorGatewayRoutesTests {
 
