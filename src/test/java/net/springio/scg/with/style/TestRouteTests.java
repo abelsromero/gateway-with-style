d1b05653b7417c29228ea920e279c96c7449eceb
Abel Salgado Romero <abelromero@gmail.com>
2023-05-16 19:06:04 +0200

    Add non-committed local configuration via "local" profile

--- src/test/java/net/springio/scg/with/style/TestRouteTests.java
+++ src/test/java/net/springio/scg/with/style/TestRouteTests.java
@@ -4,22 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
+@ActiveProfiles("local")
 @AutoConfigureWebTestClient
 class TestRouteTests {
 
     @Autowired
     WebTestClient webTestClient;
 
-	@Test
-	void testRouteIsAvailable() {
+    @Test
+    void testRouteIsAvailable() {
         webTestClient.get()
             .uri("/test/get")
             .exchange()
             .expectStatus()
             .isOk();
-	}
+    }
 
 }
